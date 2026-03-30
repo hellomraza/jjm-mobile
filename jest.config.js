fb7b030f80a54233d6f1ee32244c26c39fbad36e
hellomraza <hellomraza@gmail.com>
2026-03-30 14:55:29 +0530

    test(jest): add react-native-vector-icons to transformIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
     '^@env$': '<rootDir>/src/test/mocks/env.ts',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(@react-native|react-native|@react-navigation)/)',
+    'node_modules/(?!(@react-native|react-native|@react-navigation|react-native-vector-icons)/)',
   ],
 };
